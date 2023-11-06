515cd3c173e95fe11960b993514b81f5ddbb693f
priyanTHElearner <99947666+priyanTHElearner@users.noreply.github.com>
2023-11-07 00:05:12 +0530

    creatinga vpc

--- main.tf
+++ main.tf
@@ -18,10 +18,30 @@ data "aws_vpc" "default" {
   default = true
 }
 
+module "auto_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "priyan-private-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+ 
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
   vpc_security_group_ids = [module.priyan_sg.security_group_id]
+
+  subnet_id = module.auto_vpc.public_subnets 
 
   tags = {
     Name = "Learning Terraform"
@@ -34,7 +54,7 @@ module "priyan_sg" {
   version = "5.1.0"
   name ="priyan_new"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.auto_vpc.vpc_id
   ingress_rules        = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
